98eacb0581c9766ae01651cb3ad407c53f9afe4d
Tarik Nukic <tnukm@outlook.com>
2022-03-28 15:23:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# access-and-use-a-public-api
+ Fetching data from an API for 12 random users and use it to build an employee directory.
---
+++ js/script.js
@@ -0,0 +1,114 @@
+let employees = [];
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`;
+const gridContainer = document.querySelector('.grid-container');
+const overlay = document.querySelector('.overlay');
+const modalContainer = document.querySelector('.modal-content');
+const modalClose = document.querySelector('.modal-close');
+
+/*  
+ Fetching data for 12 random employees from the given API and calling the 'displayEmployees' function on it
+*/
+fetch(urlAPI)
+  .then(response => response.json())
+  .then(response => response.results)
+  .then(displayEmployees)
+  .catch(err => console.log(err))
+
+/*  
+ 'displayEmployees' function creates the necessary HTML markup for the 12 employees 
+*/
+function displayEmployees(employeeData) {
+    employees = employeeData;
+    let employeeHTML = '';
+
+    employees.forEach((employee, index) => {
+        let name = employee.name;
+        let email = employee.email;
+        let city = employee.location.city;
+        let picture = employee.picture;
+    
+
+        employeeHTML += `
+            <div class="card" data-index="${index}">
+                <img class="avatar" src="${picture.large}">
+                <div class="text-container">
+                    <h2 class="name">${name.first} ${name.last}</h2>
+                    <p class="email">${email}</p>
+                    <p class="address">${city}</p>
+                </div>
+            </div>
+        `;
+    });
+
+    gridContainer.innerHTML = employeeHTML;
+}
+
+/* 
+ 'displayModal' creates the markup for the modal window. It pops up when a employee card is clicked and
+ displays more information about the selected employee.
+*/
+function displayModal(index) {
+    let {name, dob,  phone, email, location: { city, street, state, postcode
+    }, picture} = employees[index];
+
+    let date = new Date(dob.date);
+
+    const modalHTML = `
+        <img class="avatar" src="${picture.large}" />
+        <div class="text-container">
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+            <hr />
+            <p>${phone}</p>
+            <p class="address">${street}, ${state}, ${postcode}</p>
+            <p>Birthday:
+            ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        </div>
+    `;
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;
+}
+
+/*  
+ Event listener which listens for 'click' events on employee cards. In this case the modal window
+ pops up and displayes more information about the selected employee.
+*/
+gridContainer.addEventListener('click', e => {
+    if (e.target !== gridContainer) {
+        const card = e.target.closest('.card');
+        const index = card.getAttribute('data-index');
+
+        displayModal(index);
+    }
+});
+
+/*  
+ Event listener which listens for 'click' events on the closing button of the modal window.
+*/
+modalClose.addEventListener('click', () => {
+    overlay.classList.add("hidden");
+    });
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
